040dfc57b686bd2adbf22c7d747cf16a59cd551d
Yumi Yukimura <me.cafebabe@gmail.com>
2025-07-04 22:02:04 +0800

    Rename to snowcastle
    Change-Id: I4bd31333de5ab4c809b54f13756c0fd7a845ff25

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,10 +1,10 @@
 #
-# SPDX-FileCopyrightText: The Wildroid Project
+# SPDX-FileCopyrightText: The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_iPhone.mk
+    $(LOCAL_DIR)/lineage_snowcastle.mk
 
 $(foreach build_type, user userdebug eng, \
-    $(eval COMMON_LUNCH_CHOICES += lineage_iPhone-$(build_type)))
+    $(eval COMMON_LUNCH_CHOICES += lineage_snowcastle-$(build_type)))
