cd2d2c502afa636feab61ede8aa2dae27673462c
kentaro aldanatien ramadhan <nkbob2707@gmail.com>
2026-07-12 02:12:33 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,7 +4,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_snowcastle.mk
+    $(LOCAL_DIR)/aetheria_snowcastle.mk
 
 $(foreach build_type, user userdebug eng, \
-    $(eval COMMON_LUNCH_CHOICES += lineage_snowcastle-$(build_type)))
+    $(eval COMMON_LUNCH_CHOICES += aetheria_snowcastle-$(build_type)))
